23dfce948d3491b0c30b1990e4fe6d9b50cbe06d
oleg_zolotar <zolotar1123@gmail.com>
2018-03-07 18:54:23 +0200

    n23 done

--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -2,17 +2,29 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+//    todoCollectionTask()
+    return customers.filter { product in it.orderedProducts }
+            .toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+//    todoCollectionTask()
+    return orders.filter { it.isDelivered }
+            .map { it.products }
+            .flatMap { it.toHashSet() }
+            .maxBy { it.price }
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+//    todoCollectionTask()
+    return customers.map { it.orders }
+            .flatMap { it.toList() }
+            .map { it.products }
+            .flatMap { it.toList() }
+            .filter { product == it  }
+            .count()
 }
